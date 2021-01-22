566fada2d4af2ec0fc0400e11cbab3fedb57753f
Karchikyan Davit <davit.karchikyan@iunetworks.am>
2021-01-22 14:50:55 +0400

    add forgot anotation for service test

--- src/test/java/am/creative/example/ExampleApplicationTest.java
+++ src/test/java/am/creative/example/ExampleApplicationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {ExampleApplication.class})
 @ActiveProfiles("test")
+@EnableAsync
 class ExampleApplicationTest {
 
     @Autowired
